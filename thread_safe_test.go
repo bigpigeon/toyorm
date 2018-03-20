cb0e759c493a6effd00dc6f4889c6a84a0f6cde3
bigpigeon <bigpigeon0@gmail.com>
2018-03-20 15:37:31 +0800

    1. update test

--- thread_safe_test.go
+++ thread_safe_test.go
@@ -15,21 +15,15 @@ import (
 func TestSameModel(t *testing.T) {
 	m1 := NewModel(reflect.TypeOf(TestPreloadTable{}))
 	m2 := NewModel(reflect.TypeOf(TestPreloadTable{}))
-	//b1, err := json.MarshalIndent(m1, "", "  ")
-	//assert.Nil(t, err)
-	//b2, err := json.MarshalIndent(m2, "", "  ")
-	//assert.Nil(t, err)
-	//
-	//t.Log(string(b1))
-	//t.Log(string(b2))
 
 	m1Value := reflect.ValueOf(*m1)
 	m2Value := reflect.ValueOf(*m2)
 	mType := m1Value.Type()
 	for i := 0; i < mType.NumField(); i++ {
 		isSame := reflect.DeepEqual(m1Value.Field(i).Interface(), m2Value.Field(i).Interface())
-		t.Logf("compare field %s : %v", mType.Field(i).Name, isSame)
+		t.Logf("compare field %s is same? %v", mType.Field(i).Name, isSame)
 		if !isSame {
+			t.Fail()
 			f1, err := json.MarshalIndent(m1Value.Field(i).Interface(), "", "  ")
 			if err != nil {
 				t.Errorf("err %s", err)
@@ -38,6 +32,7 @@ func TestSameModel(t *testing.T) {
 			if err != nil {
 				t.Errorf("err %s", err)
 			}
+
 			t.Log(string(f1))
 			t.Log(string(f2))
 		}
